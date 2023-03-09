8959c348fcb5329f8c4680468d028a3fd9a896b6
«Elvira» <elvira.salahova@gmail.com>
2023-03-09 23:22:47 +0400

    new project

---
+++ Example001/Example-Greating/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("type name of user: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "elvira")
+{
+    Console.WriteLine("Hey Hey Hey Elvira");
+}
+else
+{
+    Console.WriteLine("Hello, ");
+    Console.WriteLine(username);
+}
